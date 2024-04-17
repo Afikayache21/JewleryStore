1de6a770676e1e6d66024cc76424d3c8171424ab
Amits232 <122890393+Amits232@users.noreply.github.com>
2024-04-17 18:23:01 +0300

    home update

--- JewleryStore/Jewlery-project/src/Parts/Footer.jsx
+++ JewleryStore/Jewlery-project/src/Parts/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
     return (
         <footer >
-            <p style={{backgroundColor:'rgba(49, 49, 49)',color:"white",padding:'1%',}}>© 2024 R&N Jelery | All rights reserved | By A&A</p>
+            <p style={{backgroundColor:'rgba(49, 49, 49)',color:"white",padding:'1%',}}>© 2024 R&N jewelry  | All rights reserved | By A&A</p>
         </footer>
     )
 }
