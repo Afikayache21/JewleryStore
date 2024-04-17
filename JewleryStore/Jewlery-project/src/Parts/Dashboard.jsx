cd33d9891fe8bda866919f01574d86a0185d813a
Amits232 <122890393+Amits232@users.noreply.github.com>
2024-04-17 18:20:19 +0300

    update home

--- JewleryStore/Jewlery-project/src/Parts/Dashboard.jsx
+++ JewleryStore/Jewlery-project/src/Parts/Dashboard.jsx
@@ -4,8 +4,8 @@ import ContentCard from './ContentCard';
 import Menu from './Stam';
 import '../App.css';
 
-let card1Content = '"JavaScript is a high-level, interpreted programming language that conforms to the ECMAScript specification. It is a language that is also characterized as dynamic, weakly typed, prototype-based and multi-paradigm. HTML, on the other hand, stands for Hyper Text Markup Language. It is the standard markup language for documents designed to be displayed in a web browser. It can be assisted by technologies such as Cascading Style Sheets (CSS) and scripting languages such as JavaScript."';
-let card2Content = '"React is a high-level, interpreted programming language that conforms to the ECMAScript specification. It is a language that is also characterized as dynamic, weakly typed, prototype-based and multi-paradigm. HTML, on the other hand, stands for Hyper Text Markup Language. It is the standard markup language for documents designed to be displayed in a web browser. It can be assisted by technologies such as Cascading Style Sheets (CSS) and scripting languages such as JavaScript."';
+let card1Content = 'Welcome to R&A, where elegance meets craftsmanship in a symphony of precious metals. Nestled in the heart of the city, our boutique showcases a breathtaking array of rings, necklaces, and earrings, each piece meticulously crafted to captivate the soul. Explore our curated collection featuring timeless designs in lustrous gold, sleek silver, and enchanting rose gold, each exuding its own unique allure. From delicate necklaces that grace the neckline with understated sophistication to statement rings that command attention, every creation at R&A embodies a harmonious blend of tradition and contemporary flair. Whether you seek a symbol of enduring love or a shimmering accent to elevate your ensemble, R&A invites you to discover the perfect expression of your style and grace.';
+let card2Content = `Discover a seamless journey of indulgence with R&A's online collection. Explore exquisite jewelry in gold, silver, and rose gold. Enjoy easy browsing, secure transactions, and worldwide shipping. Adorn yourself with elegance from R&A.`;
 
 const Dashboard = () => {
     const [activeTab, setActiveTab] = useState(0);
@@ -20,23 +20,21 @@ const Dashboard = () => {
                 
                 <Nav variant="tabs" dir="rtl"  >
                     <Nav.Item>
-                        <Nav.Link onClick={() => handleTabClick(0)}>Card-1</Nav.Link>
+                        <Nav.Link onClick={() => handleTabClick(0)}>About Us</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
                         <Nav.Link onClick={() => handleTabClick(1)} >Card-2</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Nav.Link onClick={() => handleTabClick(2)}>
-                            Card-3
-                        </Nav.Link>
+                        <Nav.Link onClick={() => handleTabClick(2)}>?Why Shop Online</Nav.Link>
                     </Nav.Item>
                 </Nav>
 
             </div>
             <div>
-                {activeTab === 0 && <h2><ContentCard content={card1Content} title={'Card-1'} /></h2>}
+                {activeTab === 0 && <h2><ContentCard content={card1Content} title={'About Us'} /></h2>}
                 {activeTab === 1 && <h2><ContentCard content={menu} title={'Card-2'} /></h2>}
-                {activeTab === 2 && <h2><ContentCard content={card2Content} title={'Card-3'} /></h2>}
+                {activeTab === 2 && <h2><ContentCard content={card2Content} title={'Why Shop Online?'} /></h2>}
             </div>
         </>
     );
